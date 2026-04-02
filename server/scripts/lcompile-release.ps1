f9275788c4cf04e087e1e47f907adda816304223
fanlusky <1730976608@qq.com>
2026-04-02 18:25:57 +0800

    add compile release scripts

---
+++ server/scripts/lcompile-release.ps1
@@ -0,0 +1,41 @@
+#
+# generate, compile and run exe files
+#
+function getExePathFromCMakeLists([string]$buildDir, [string]$config) {
+    $content = Get-Content -Raw -Path "./CMakeLists.txt"
+    $exePath = ""
+    foreach ($line in $content -split "`n") {
+        if ($line -match 'set\(MY_EXECUTABLE_NAME[^\"]*\"([^\"]+)\"') {
+            $exeName = $matches[1]
+            $exePath = Join-Path -Path $buildDir -ChildPath ("bin/{0}/{1}.exe" -f $config, $exeName)
+            break
+        }
+    }
+    return $exePath
+}
+
+$currentDirectory = Get-Location
+$cmakeListsPath = Join-Path -Path $currentDirectory -ChildPath "CMakeLists.txt"
+
+if (-not (Test-Path $cmakeListsPath)) {
+    Write-Host("No CMakeLists.txt in current directory, please check.")
+    return
+}
+
+Write-Host "Start generating and compiling..."
+
+$buildFolderPath = ".\build-release"
+$presetName = "vcpkg-release"
+$buildConfig = "Release"
+
+if (-not (Test-Path $buildFolderPath)) {
+    New-Item -ItemType Directory -Path $buildFolderPath | Out-Null
+    Write-Host "build-release folder created."
+}
+
+# cmake -G "Visual Studio 18 2026" -A x64 -S . -B ./build-release
+cmake --preset=$presetName
+
+if ($LASTEXITCODE -eq 0) {
+    cmake --build $buildFolderPath --config $buildConfig
+}
