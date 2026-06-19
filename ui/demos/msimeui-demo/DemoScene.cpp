6326e0f81dcc0a2442e901c70c57f3cf43b42026
fanlusky <1730976608@qq.com>
2026-06-19 11:42:18 +0800

    Remove candidate header bottom gap

--- ui/demos/msimeui-demo/DemoScene.cpp
+++ ui/demos/msimeui-demo/DemoScene.cpp
@@ -465,7 +465,7 @@ std::unique_ptr<Scene> CreateDemoScene()
     auto candidatePinyin = std::make_shared<TextBlock>(L"ni", 15.2f, D2D1::ColorF(0xFFFFFF), false);
     candidatePinyin->SetFontFamily(L"Noto Sans SC");
     candidatePinyin->SetTextLayoutPadding({0.0f, 0.0f, 0.0f, 0.0f});
-    candidatePinyin->SetMargin({12.0f, 0.0f, 0.0f, 2.0f});
+    candidatePinyin->SetMargin({12.0f, 0.0f, 0.0f, 0.0f});
     candidatePopupStack->AddChild(candidatePinyin);
 
     auto candidateList = std::make_shared<CandidateList>(25.0f);
